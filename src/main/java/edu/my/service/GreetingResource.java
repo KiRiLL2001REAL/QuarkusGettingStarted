af6d28fd7e26d18a0364054ac62bd8fc304c8cf8
Kirill Baturin <batyrin2001@yandex.ru>
2023-07-07 02:33:27 +0300

    added GreetingService and its test

--- src/main/java/edu/my/service/GreetingResource.java
+++ src/main/java/edu/my/service/GreetingResource.java
@@ -1,12 +1,24 @@
 package edu.my.service;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    GreetingService serviceGreeting;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(@PathParam("name") String name) {
+        return serviceGreeting.greeting(name);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
--- src/test/java/edu/my/service/GreetingResourceTest.java
+++ src/test/java/edu/my/service/GreetingResourceTest.java
@@ -3,6 +3,8 @@ package edu.my.service;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -12,10 +14,21 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+                .when().get("/hello")
+                .then()
+                    .statusCode(200)
+                    .body(is("Hello from RESTEasy Reactive"));
+    }
+
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+                .pathParam("name", uuid)
+                .when().get("/hello/greeting/{name}")
+                .then()
+                    .statusCode(200)
+                    .body(is("Hello, dear " + uuid + "."));
     }
 
 }
