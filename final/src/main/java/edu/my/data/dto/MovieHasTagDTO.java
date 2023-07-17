306cd6f96b8085f036796450a4533f83a4bc1349
Kirill Baturin <batyrin2001@yandex.ru>
2023-07-17 05:32:35 +0300

    Adding @Shema annotation to DTOs

--- final/src/main/java/edu/my/data/dto/MovieHasTagDTO.java
+++ final/src/main/java/edu/my/data/dto/MovieHasTagDTO.java
@@ -1,11 +1,15 @@
 package edu.my.data.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 public class MovieHasTagDTO {
+    @Schema(title = "Link id", required = true)
     public Long id;
     @NotBlank
+    @Schema(title = "The movie referenced by this link", required = false)
     public MovieDTO movie;
     @NotBlank
+    @Schema(title = "The tag referenced by this link", required = false)
     public TagDTO tag;
 }
--- final/src/main/java/edu/my/data/dto/TagDTO.java
+++ final/src/main/java/edu/my/data/dto/TagDTO.java
@@ -2,14 +2,18 @@ package edu.my.data.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotBlank;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.util.Set;
 
 public class TagDTO {
+    @Schema(title = "Tag id", required = true)
     public Long id;
     @NotBlank
+    @Schema(title = "Tag description", required = true)
     public String name;
 
+    @Schema(title = "Link of the tag with the films that include it", required = false)
     @JsonIgnore
     Set<MovieHasTagDTO> tagHasMovieSet;
 }
