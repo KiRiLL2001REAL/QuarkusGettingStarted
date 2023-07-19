dffb19c2e6269ba0bd0b6213abb35b0e64cdf391
Kirill Baturin <batyrin2001@yandex.ru>
2023-07-20 02:23:27 +0300

    Changed the way of make database healthcheck.

--- final/src/main/java/edu/my/health/DatabaseCheck.java
+++ final/src/main/java/edu/my/health/DatabaseCheck.java
@@ -1,42 +1,28 @@
 package edu.my.health;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
 import org.eclipse.microprofile.health.Liveness;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 @Liveness
 public class DatabaseCheck implements HealthCheck {
-
-    @ConfigProperty(name = "quarkus.datasource.jdbc.url")
-    String db_url;
-    @ConfigProperty(name = "quarkus.datasource.username")
-    String db_user;
-    @ConfigProperty(name = "quarkus.datasource.password")
-    String db_passwd;
-
-    Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    private boolean isConnected() {
-        try (Connection connection = DriverManager.getConnection(db_url, db_user, db_passwd)) {
-            if (connection != null)
-                return true;
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-        }
-        return false;
-    }
+    @Inject
+    EntityManager entityManager;
 
     @Override
     public HealthCheckResponse call() {
-        if (isConnected()){
-            return HealthCheckResponse.up("Database is alive :)");
+        HealthCheckResponseBuilder responseBuilder;
+
+        try {
+            entityManager.createQuery("SELECT 1").getResultList();
+            responseBuilder = HealthCheckResponse.named("Database is alive :)").up();
+        } catch (Exception e) {
+            responseBuilder = HealthCheckResponse.named("Database is dead :'(").down();
         }
-        return HealthCheckResponse.down("Database is dead :'(");
+
+        return responseBuilder.build();
     }
 }
